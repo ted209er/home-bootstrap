5a64410a79ffcf5bc3909fb0acf585396b610420
ted209er <ted209er@gmail.com>
2025-07-14 20:14:40 -0500

    adding oh-my-zsh plugins

--- bootstrap.sh
+++ bootstrap.sh
@@ -37,7 +37,7 @@ fi
 [ ! -d "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting" ] && git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
 
 # Install powerlevel10k theme
-[ ! -d "${ZSH_CUSTOM}/themes/powerlevel10k" ] && git clone ---depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k"
+[ ! -d "${ZSH_CUSTOM}/themes/powerlevel10k" ] && git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM}/themes/powerlevel10k"
 
 
 # Set zsh as default shell
